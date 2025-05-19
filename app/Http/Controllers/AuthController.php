b91a2883e34fbc824ffcc55deb402331f962ce17
swostik720 <paneruswostik@gmail.com>
2025-05-19 14:36:43 +0545

    password issue resolved

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -70,7 +70,7 @@ class AuthController extends Controller
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
-            'password' => Hash::make($request->password),
+            'password' => $request->password,
             'role' => 'user', // Default role is user
         ]);
 
